b85a481cae9dfa243967867b52f6f5c68c04ec6f
arpit <piparaarpit23@gmail.com>
2017-11-26 19:59:16 +0530

    First commit

--- IOT/app/src/main/java/com/ontosomething/arpit/iot/Constant.java
+++ IOT/app/src/main/java/com/ontosomething/arpit/iot/Constant.java
@@ -9,3 +9,7 @@ public class Constant {
     public static String APP_GUI_ID= "443443f0-6450-4591-a4fd-51c3f08d1d8c";
     public static String CLIENT_SECRET="4921a548-407a-4db5-b70b-31b5bdd0552";
 }
+
+
+
+
